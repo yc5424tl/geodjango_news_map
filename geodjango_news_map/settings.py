f2ca19febf220cccdc7dfbec8a343ce73bbbfcb4
yc5424tl <jkboline@gmail.com>
2019-09-28 15:20:32 -0500

    Minor Changes for Heroku Deploy

--- geodjango_news_map/settings.py
+++ geodjango_news_map/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geodjango_news_map_web',
-    #'geodjango_news_map_web.apps.PostgisNewsMapDockerConfig',
 ]
 
 MIDDLEWARE = [
