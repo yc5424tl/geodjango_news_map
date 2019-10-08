7d92835379c754f8c62599e8b474e9461cce25d9
yc5424tl <jkboline@gmail.com>
2019-10-08 10:44:24 -0500

    Adding AWS S3 Storage Backend for static and media files.

--- geodjango_news_map_web/views.py
+++ geodjango_news_map_web/views.py
@@ -109,7 +109,7 @@ def new_query(request):
         # qrs.choropleth.save(ContentFile(data_tup[0]))
         file_data = qrs.choro_html.encode()
         f = io.BytesIO(file_data)
-        qrs.choropleth.save(ContentFile(f.read()))
+        qrs.choropleth.save(name=qrs.filename, content=ContentFile(f.read()))
         qrs.save()
         s3_path = qrs.choropleth.url
         logger.debug(f'qrs.choropleth.url => {qrs.choropleth.url}')
