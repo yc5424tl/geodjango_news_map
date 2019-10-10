f313954b81b84b0b08acda427da393b7898342c0
yc5424tl <jkboline@gmail.com>
2019-10-10 01:23:52 -0500

    Starting on responsiveness to screen sizes.

--- geodjango_news_map_web/static/js/base.js
+++ geodjango_news_map_web/static/js/base.js
@@ -9,26 +9,22 @@ $(window).onresize(() => {
    let nav_img = $('#nav-img');
 
    if (win.width() < 1562) {
-       if ($(top_img_div).hasClass('hidden')) {
-           $(top_img_div).removeClass('hidden');
-       }
+       $(top_img_div).show();
+       $(nav_ctr).hide();
+       $(nav_img).hide();
        if ($(nav_left).hasClass('col-4')) {
            $(nav_left).removeClass('col-4').addClass('col');
            $(nav_right).removeClass('col-4').addClass('col');
        }
-       $(nav_ctr).addClass('hidden');
-       $(nav_img).addClass('hidden');
    }
 
    if (win.width() > 1561) {
-       if($(nav_ctr).hasClass('hidden')) {
-           $(nav_ctr).removeClass('hidden');
-           $(nav_img).removeClass('hidden');
-       }
+       $(top_img_div).hide();
+       $(nav_ctr).show();
+       $(nav_img).show();
        if($(nav_left).hasClass('col')) {
            $(nav_left).removeClass('col').addClass('col-4');
            $(nav_right).removeClass('col').addClass('col-4');
        }
-       $(top_img_div).addClass('hidden')
    }
 });
