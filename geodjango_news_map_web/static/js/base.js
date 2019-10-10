ef9062ef37fb4c23379e7b4b1e60c06489a804e5
yc5424tl <jkboline@gmail.com>
2019-10-10 01:41:16 -0500

    Starting on responsiveness to screen sizes.

--- geodjango_news_map_web/static/js/base.js
+++ geodjango_news_map_web/static/js/base.js
@@ -1,30 +1,33 @@
 // $('').addClass('');
 
-$(window).onresize(() => {
-   let win = $(this);
-   let top_img_div = $('#top-nav-img-div');
-   let nav_left = $('#nav-lft');
-   let nav_ctr = $('#nav-ctr');
-   let nav_right = $('#nav-rgt');
-   let nav_img = $('#nav-img');
+setLayout = function() {
 
-   if (win.width() < 1562) {
-       $(top_img_div).show();
-       $(nav_ctr).hide();
-       $(nav_img).hide();
-       if ($(nav_left).hasClass('col-4')) {
-           $(nav_left).removeClass('col-4').addClass('col');
-           $(nav_right).removeClass('col-4').addClass('col');
-       }
-   }
+    let top_img_div = $('#top-nav-img-div');
+    let nav_left = $('#nav-lft');
+    let nav_ctr = $('#nav-ctr');
+    let nav_right = $('#nav-rgt');
+    let nav_img = $('#nav-img');
 
-   if (win.width() > 1561) {
-       $(top_img_div).hide();
-       $(nav_ctr).show();
-       $(nav_img).show();
-       if($(nav_left).hasClass('col')) {
-           $(nav_left).removeClass('col').addClass('col-4');
-           $(nav_right).removeClass('col').addClass('col-4');
-       }
-   }
-});
+    if (window.width() < 1562) {
+        $(top_img_div).show();
+        $(nav_ctr).hide();
+        $(nav_img).hide();
+        if ($(nav_left).hasClass('col-4')) {
+            $(nav_left).removeClass('col-4').addClass('col');
+            $(nav_right).removeClass('col-4').addClass('col');
+        }
+    }
+
+    if (window.width() > 1561) {
+        $(top_img_div).hide();
+        $(nav_ctr).show();
+        $(nav_img).show();
+        if ($(nav_left).hasClass('col')) {
+            $(nav_left).removeClass('col').addClass('col-4');
+            $(nav_right).removeClass('col').addClass('col-4');
+        }
+    }
+};
+
+$(window).onresize(setLayout);
+$(document).onLoad(setLayout);
