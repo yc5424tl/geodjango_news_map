d63667f2716f27b5226b581b0332256563124958
yc5424tl <jkboline@gmail.com>
2019-10-12 17:01:09 -0500

    Starting on responsiveness to screen sizes.

--- geodjango_news_map_web/static/js/base.js
+++ geodjango_news_map_web/static/js/base.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
 setLayout = function() {
     let md_nav_div = $('#md-nav-div');
     let lg_nav_div = $('#lg-nav-div');
+    let lg_nav_img = $('#lg-nav-img');
+    let lg_nav_content = $('#nav-content-lg');
     let md_nav_img_div = $('#md-nav-img-div');
     let md_nav_col_div = $('#md-nav-col-div');
     let md_nav_div_desc = $('#md-nav-div *');
@@ -24,6 +26,8 @@ setLayout = function() {
 
     if ($(window).width() > 1561) {
         lg_nav_div.css('display', 'flex');
+        lg_nav_content.css('display', 'block');
+        lg_nav_img.css('display', 'block');
         md_nav_div.css('display', 'none');
         md_nav_div_desc('display', 'none');
         // // nav.css('height', '30vh');
@@ -41,6 +45,8 @@ setLayout = function() {
 
     if ($(window).width() < 1562) {
         lg_nav_div.css('display', 'none');
+        lg_nav_img.css('display', 'none');
+        lg_nav_content.css('display', 'none');
         md_nav_div.css('display', 'flex');
         md_nav_div_desc('display', 'unset');
         md_nav_col_div.css('display', 'block');
