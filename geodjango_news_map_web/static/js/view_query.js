fa4ee16507324927e275555e83ed24a1e8847676
yc5424tl <jkboline@gmail.com>
2019-10-29 05:05:02 -0500

    modified view_sources style along with root

--- geodjango_news_map_web/static/js/view_query.js
+++ geodjango_news_map_web/static/js/view_query.js
@@ -1,16 +1,20 @@
 
-let preTextarea = $('#pre-textarea');
-
-preTextarea.on('focus', () =>  {
-    if($(this).textContent.toLowerCase().replace(`<br>`, "").trim().match('share your thoughts!')) {
-        $(this).textContent = "";
-        $(this).css('color', '#35e8d6');
-    }
+$(document).ready(() => {
+    let preTextarea = $('#pre-textarea');
+    preTextarea.on({
+        'focus': () => {
+            if ($(this).textContent.toLowerCase().replace(`<br>`, "").trim().match('share your thoughts!')) {
+                $(this).textContent = "";
+                $(this).css('color', '#35e8d6');
+            }
+        },
+        'focusout': () => {
+            if ($(this).textContent.replace(`<br>`, "").trim().match("")) {
+                $(this).text('Share Your Thoughts!');
+                $(this).css('color', '#424B54');
+            }
+        }}
+    )
 });
 
-preTextarea.on('focusout', () => {
-    if($(this).textContent.replace(`<br>`, "").trim().match("")) {
-        $(this).text('Share Your Thoughts!');
-        $(this).css('color', '#424B54');
-    }
-});
+
