4c32e322972ebbd37fa5e6e95f17e602fae03e38
yc5424tl <jkboline@gmail.com>
2019-10-29 03:59:05 -0500

    modified view_sources style along with root

--- geodjango_news_map_web/static/js/view_query.js
+++ geodjango_news_map_web/static/js/view_query.js
@@ -2,15 +2,15 @@
 let preTextarea = $('#pre-textarea');
 
 preTextarea.on('focus', () =>  {
-    if($(this).text().toLowerCase().match('share your thoughts!')) {
-        $(this).text('');
-        $(this).css('color', 'var(--aux-accent-6)');
+    if($(this).textContent.toLowerCase().replace(`<br>`, "").trim().match('share your thoughts!')) {
+        $(this).textContent = "";
+        $(this).css('color', '#35e8d6');
     }
 });
 
 preTextarea.on('focusout', () => {
-    if($(this).text().match('')) {
+    if($(this).textContent.replace(`<br>`, "").trim().match("")) {
         $(this).text('Share Your Thoughts!');
-        $(this).css('color', 'var(--main-grey)');
+        $(this).css('color', '#424B54');
     }
 });
