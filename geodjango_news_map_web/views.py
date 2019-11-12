ce20ac5b4c44ab1a6a5d58867f80a6866cd70756
yc5424tl <jkboline@gmail.com>
2019-11-12 02:43:00 -0600

    Made css files more readable
    in mock.py: adding functionality to check list of dicts containing source data to add new categories to a source when found.
    
    In the works: changing from writing to file with each new source/update, to writing once all sources have been processed.

--- geodjango_news_map_web/views.py
+++ geodjango_news_map_web/views.py
@@ -372,12 +372,11 @@ def import_sources(request):
         #     payload_dict = json.loads(request.POST.get('data'))
         #     logger.log(level=logging.INFO, msg=f'USER IS AUTHENTICATED\n\nJSON DATA FROM POST ->\n\n {payload_dict}')
         # payload_dict = json.loads(request.POST.get('data'))
-        payload_1 = request.get_json()
+
         payload_3 = json.loads(request.body.decode("utf-8"))
         payload_2 = json.loads(request.POST.get('data'))
         logger.log(level=logging.INFO, msg=f'type(json_payload) == {type(payload_2(2))}')
         # logger.log(level=logging.INFO, msg=f'USER IS AUTHENTICATED\n\nJSON DATA FROM POST ->\n\n {json_payload}')
-        logger.log(level=logging.DEBUG, msg=f'type(payload_1) == {type(payload_1)} == {payload_1}')
         logger.log(level=logging.DEBUG, msg=f'type(payload_2) == {type(payload_2)} == {payload_2}')
         logger.log(level=logging.DEBUG, msg=f'type(payload_3) == {type(payload_3)} == {payload_3}')
         try:
