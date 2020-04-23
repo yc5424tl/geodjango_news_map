9e9063635704d042de30e1d457f09040c41d2792
j <j@localhost.com>
2020-04-23 08:45:35 -0500

    STATIC_URL = '/staticfiles/' not 'staticfiles'

--- geodjango_news_map/settings.py
+++ geodjango_news_map/settings.py
@@ -160,9 +160,9 @@ USE_TZ = True
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_S3_ID', "")
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SEC', "")
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', "")
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_S3_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SEC')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
@@ -170,7 +170,7 @@ if USE_S3:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
-    STATIC_URL = 'staticfiles'
+    STATIC_URL = '/staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
