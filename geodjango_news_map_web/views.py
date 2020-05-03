8953f5f927b04d8eae98254e29d928e971c4a5a2
yc5424tl <yc5424tl@go.minneapolis.edu>
2020-05-03 03:39:23 -0500

    new s3 bucket, group, and user configured

--- geodjango_news_map_web/views.py
+++ geodjango_news_map_web/views.py
@@ -379,14 +379,14 @@ def view_choro(request: requests.request, query_pk) -> render:
     qrs = QueryResultSet.objects.get(pk=query_pk)
     return render(request, 'general/view_choro.html', {'query': qrs})
 
-def handler404c(request):
+def handler404(request):
     return page_not_found(request, template_name="errors/404.html")
   #  response = render_to_response('error/404.html')
   #  response.status_code = 404
  #   return response
     # return render(request, 'error/404.html', status=404)
 
-def handler500c(request):
+def handler500(request):
     return server_error(request, template_name="errors/500.html")
   #  response = render_to_response('error/500.html')
   #  response.status_code = 500
