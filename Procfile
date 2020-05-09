ce9c8a1ee8a5219ca5fe15de3a730cfa697f842f
yc5424tl <yc5424tl@go.minneapolis.edu>
2020-05-09 08:27:12 -0500

    init commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn geodjango_news_map.wsgi --log-file -
--- geodjango_news_map_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostgisNewsMapDockerConfig(AppConfig):
-    name = 'geodjango_news_map_web'
